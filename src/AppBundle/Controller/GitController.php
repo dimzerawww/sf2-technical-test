cd1a817259001d35cabc4fcdede0c2064f4f5ba8
dimzerawww <dimzeraw@gmail.com>
2016-01-08 03:08:20 +0100

    Debut gestion commentaire + verif depot + optims

--- src/AppBundle/Controller/GitController.php
+++ src/AppBundle/Controller/GitController.php
@@ -18,13 +18,13 @@ class GitController extends Controller
         ));
     }
 
-    public function checkUserAction(Request $user){
+    public function checkUserAction(Request $request){
 
         if (is_string($_POST['git_user'])){
-            if ($this->performUrlRequest($user)->total_count > 0){ // Si la requête GitHub retourne des résultats
+            if ($this->performUrlRequest($request)->total_count > 0){ // Si la requête GitHub retourne des résultats
                 // Redirection vers l'url personnalisée en fonction du nom d'utilisateur
                 return $this->redirect(
-                    $this->generateUrl("git_username", array("git_username" => $_POST['git_user']))
+                    $this->generateUrl("git_username", array("git_username" => $request->request->get('git_user')))
                 );
             }
             else {
@@ -42,10 +42,48 @@ class GitController extends Controller
         }
     }
 
-    public function performUrlRequest(Request $user){
+    public function checkRepositoryAction(Request $request){
+        /* Récupération des dépôts sur GitHub avec curl (pour vérification) */
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/users/'.$request->request->get('git_username').'/repos');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.13'); // Forge un User Agent Mozilla (sécurité API Git)
+
+        $response = curl_exec($ch);  // Récupération de la réponse
+        $data = json_decode($response); // Décodage de la réponse
+
+        $totalRepos = count($data);
+        $tmpCount = 0;
+
+        /* On parcours l'ensemble des dépôts */
+        foreach($data as $repo){
+            if ($repo->full_name == $request->request->get('git_repository')){
+                /* actions à effectuer (projet non terminé) */
+                break;
+            }
+            else {
+                $tmpCount++;
+            }
+        }
+        if ($tmpCount == $totalRepos){
+            // Affichage du template comment.html.twig
+            return $this->render('AppBundle:Git:comment.html.twig', array(
+                'git_username' => $request->request->get('git_username'),
+                'error' => "Aucun dépôt de ce nom lié à cet utilisateur",
+            ));  
+        }
+        else {
+            return $this->render('AppBundle:Git:comment.html.twig', array(
+                'git_username' => $request->request->get('git_username'),
+                'error' => "Dépôt trouvé, vous pouvez ajouter un commentaire (not done !)",
+            ));  
+        }
+    }
+
+    public function performUrlRequest(Request $request){
         /* Recherche de l'utilisateur sur GitHub avec curl */
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/search/users?q='.$_POST['git_user']);
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/search/users?q='.$request->request->get('git_user'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.13'); // Forge un User Agent Mozilla (sécurité API Git)
 
@@ -59,6 +97,7 @@ class GitController extends Controller
     {
         // Affichage du template comment.html.twig
         return $this->render('AppBundle:Git:comment.html.twig', array(
+            'git_username' => $git_username,
             'error' => "",
         ));
     }
