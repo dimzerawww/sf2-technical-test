e2dd285ede6df59593be13dbdd960411dd5354c8
dimzerawww <dimzeraw@gmail.com>
2016-01-08 02:03:15 +0100

    Ajout formulaire Git + requete API + verification

--- src/AppBundle/Controller/GitController.php
+++ src/AppBundle/Controller/GitController.php
@@ -13,9 +13,42 @@ class GitController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ]);
+        // Affichage du template index.html.twig
+        return $this->render('AppBundle:Git:index.html.twig', array(
+        ));
+    }
+
+    public function checkUserAction(Request $user){
+
+        if (is_string($_POST['git_user'])){
+            if ($this->performUrlRequest($user)->total_count > 0){ // Si la requête GitHub retourne des résultats
+                return "bien ouej";
+            }
+            else {
+                $error = "Compte GitHub non trouvé";
+                return $this->render('AppBundle:Git:index.html.twig', array(
+                    'error'         => $error,
+                ));
+            }
+        }
+        else {
+            $error = "Le champ saisi n'est pas une chaine de caractères";
+            return $this->render('AppBundle:Git:index.html.twig', array(
+                'error'         => $error,
+            ));
+        }
+    }
+
+    public function performUrlRequest(Request $user){
+        /* Recherche de l'utilisateur sur GitHub avec curl */
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/search/users?q='.$_POST['git_user']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.13'); // Forge un User Agent Mozilla (sécurité API Git)
+
+        $response = curl_exec($ch);  // Récupération de la réponse
+        $data = json_decode($response); // Décodage de la réponse
+
+        return $data;
     }
 }
--- src/StadLineUserBundle/Controller/SecurityController.php
+++ src/StadLineUserBundle/Controller/SecurityController.php
@@ -12,7 +12,7 @@ class SecurityController extends Controller
   {
     // Si le visiteur est déjà identifié, on le redirige vers l'accueil
     if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-      return $this->redirectToRoute('git_home');
+      return $this->redirectToRoute('git');
     }
     
     // Le service authentication_utils permet de récupérer le nom d'utilisateur
